a5e9f9e4ef2a3a13fd55a1a424aa5bb0da280823
AungZinTun <42179343+AungZinTun@users.noreply.github.com>
2019-10-10 02:38:56 +0630

    home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -2,19 +2,33 @@
   <div class="home">
     <div class="container mt-2">
       <!-- {{notify}} -->
-    {{notify}}
-          <li v-for="noti in notify" v-bind:key="noti.id">
-           {{ noti.name }}
-          </li>
+      <!-- {{notify}} -->
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">name</th>
+            <th scope="col">phone</th>
+            <th scope="col">age</th>
+            <th scope="col">sex</th>
+            <th scope="col">township</th>
+            <th scope="col">specimen_collected_date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="noti in notify" v-bind:key="noti.id">
+            <td>{{ noti.name }}</td>
+            <td>{{noti.phone}}</td>
+            <td>{{noti.age}}</td>
+            <td>{{noti.sex}}</td>
+            <td>{{noti.township}}</td>
+            <td>{{noti.specimen_collected_date}}</td>
+          </tr>
+        </tbody>
+      </table>
 
       <div class="my-4">
         <p v-show="LoadingNotify">...loading...</p>
-        <button
-          v-show="next"
-          @click="getnotify"
-          class="btn btn-sm btn-outline-success"
-          >Load More
-        </button>
+        <button v-show="next" @click="getNotifies" class="btn btn-sm btn-outline-success">Load More</button>
       </div>
     </div>
   </div>
@@ -29,7 +43,7 @@ export default {
       notify: [],
       next: null,
       LoadingNotify: false
-    }
+    };
   },
   methods: {
     getNotifies() {
@@ -39,23 +53,22 @@ export default {
         endpoint = this.next;
       }
       this.LoadingNotify = true;
-      apiService(endpoint)
-        .then(data => {
-          this.notify.push(...data.results)
-          this.LoadingNotify = false;
+      apiService(endpoint).then(data => {
+        this.notify.push(...data.results);
+        this.LoadingNotify = false;
 
-          if (data.next) {
-            this.next = data.next;
-          } else {
-            this.next = null;
-          }
-        })
+        if (data.next) {
+          this.next = data.next;
+        } else {
+          this.next = null;
+        }
+      });
     }
   },
   created() {
-    this.getNotifies()
+    this.getNotifies();
     document.title = "Notify Register";
-    // console.log(this.notify);
+    console.log(this.notify);
   }
 };
 </script>
@@ -63,7 +76,7 @@ export default {
 <style scoped>
 .notify-author {
   font-weight: bold;
-  color: #DC3545;
+  color: #dc3545;
 }
 
 .notify-link {
@@ -72,7 +85,7 @@ export default {
 }
 
 .notify-link:hover {
-  color: #343A40;
+  color: #343a40;
   text-decoration: none;
 }
 </style>
